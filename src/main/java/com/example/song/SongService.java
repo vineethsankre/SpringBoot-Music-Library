c56d9fd633a2d69f63cd0fa8daee93d3c0b339be
vineethsankre <lsvineethsankre@gmail.com>
2023-12-15 13:32:12 +0000

    First Commit

---
+++ src/main/java/com/example/song/SongService.java
@@ -0,0 +1,33 @@
+/*
+
+ * You can use the following import statements
+  
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.web.server.ResponseStatusException;
+
+ */
+
+package com.example.song;
+
+import java.util.*;
+
+import com.example.song.Song;
+import com.example.song.SongRepository;
+
+// Don't modify the below code
+public class SongService implements SongRepository {
+    private static HashMap<Integer, Song> playlist = new HashMap<>();
+
+    public SongService() {
+        playlist.put(1, new Song(1, "Butta Bomma", "Ramajogayya Sastry", "Armaan Malik", "Thaman S"));
+        playlist.put(2, new Song(2, "Kathari Poovazhagi", "Vijay", "Benny Dayal, Swetha Mohan", "A.R. Rahman"));
+        playlist.put(3, new Song(3, "Tum Hi Ho", "Mithoon", "Arijit Singh", "Mithoon"));
+        playlist.put(4, new Song(4, "Vizhiyil", "Vairamuthu", "Unni Menon", "A.R. Rahman"));
+        playlist.put(5, new Song(5, "Nenjame", "Panchu Arunachalam", "S.P.Balasubrahmanyam", "Ilaiyaraaja"));
+    }
+
+    // Don't modify the above code
+
+    // Write your code here
+
+}
